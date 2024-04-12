744dfa61569293f92e9610f3c91adaa6787ed8ab
Adrian <dmitrukadrian@gmail.com>
2024-04-12 16:40:03 +0300

    feat: fix cors index

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import cors from "cors";
 import express from "express";
-
 import mongoose from "mongoose";
 
 import {
@@ -24,8 +22,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: "*" }));
-app.options("*", cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.get("/api/person", getPersons);
 app.post(
