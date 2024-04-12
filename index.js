b9a6cae358666c3ea9a3bd32b065ca11c3c376f9
Adrian <dmitrukadrian@gmail.com>
2024-04-12 16:14:15 +0300

    feat: fix cors 4

--- index.js
+++ index.js
@@ -24,14 +24,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors({ origin: "*" }));
 
 app.get("/api/person", getPersons);
 app.post(
